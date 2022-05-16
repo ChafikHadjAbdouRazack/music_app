e782416aa922f75a0cd2d82c52e4c26f5afd5644
Chafik HADJ ABDOU RAZACK <chafikhadjabdourazack@gmail.com>
2022-05-16 16:36:56 +0100

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'music.dart';
+import 'package:audioplayers/audioplayers.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -7,24 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the applicati on
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Music App'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -32,14 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +34,157 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Music> myMusics = [
+    Music("J'ai dit non",'Dadju','images/dadju.jpeg','songs/Dadju_J_ai_dit_non.mp3'),
+    Music('Comme un nombre','Gims','images/gims.jpeg','songs/01_Comme_une_ombre.mp3')
+  ];
+  late StreamSubscription positionsub;
+  late StreamSubscription stateSub;
+  AudioPlayer audioPlayer = AudioPlayer();
+  Duration position = Duration(seconds: 0);
+  Duration duration = Duration(seconds: 0);
+  PlayerState state = PlayerState.STOPPED;
+  late Music myCurrentMusic;
+  @override
+  void initState(){
+    super.initState();
+    myCurrentMusic = myMusics[0];
+    audioPlayerConfiguration();
+  }
+  @override
+  Widget build(BuildContext context) {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.grey[900],
+        title: Text(widget.title),
+      ),
+      backgroundColor: Colors.grey[800],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Card(
+              elevation: 9.0,
+              child: Container(
+                width: MediaQuery.of(context).size.height/2.5,
+                child: Image.asset(myCurrentMusic.imagePath),
+              ),
+            ),
+            textWithStyle(myCurrentMusic.title, 1.5),
+            textWithStyle(myCurrentMusic.artist, 1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                button(Icons.fast_rewind, 30.0, ActionMusic.rewind),
+                button(state == PlayerState.PLAYING ? Icons.pause : Icons.play_arrow, 45.0, state == PlayerState.PLAYING  ? ActionMusic.pause : ActionMusic.play ),
+                button(Icons.fast_forward, 30.0, ActionMusic.forward)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                textWithStyle('0:0', 0.8),
+                textWithStyle('0:22', 0.8),
+              ],
+            ),
+            Slider(
+                value: position.inSeconds.toDouble(),
+                min: 0.0,
+                max: 30.0,
+                inactiveColor: Colors.white,
+                activeColor: Colors.red,
+                onChanged: (double d){
+                    setState((){
+                      position = Duration(seconds: d.toInt());
+                    });
+                }
+            )
+          ],
+
+        ),
+      )
+    );
+  }
+  Text textWithStyle(String data,double scale){
+    return Text(
+      data,
+      textScaleFactor: scale,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+        fontStyle: FontStyle.italic
+      ),
+    );
+  }
+
+  IconButton button(IconData icon,double taille,ActionMusic action){
+        return IconButton(
+          iconSize: taille,
+           color: Colors.white,
+            onPressed: (){
+              switch(action){
+                case ActionMusic.play:
+                  play();
+                  break;
+                case ActionMusic.pause:
+                  pause();
+                  break;
+                case ActionMusic.rewind:
+                  break;
+                case ActionMusic.forward:
+                  break;
+              }
+            },
+            icon: Icon(icon)
+        );
+  }
+  void audioPlayerConfiguration(){
+    positionsub = audioPlayer.onAudioPositionChanged.listen((event) {
+                setState(
+                    ()=>position = event
+                );
+    });
+    stateSub = audioPlayer.onPlayerStateChanged.listen((event) {
+      if(event == PlayerState.PLAYING){
+        setState( () async {
+          duration  =  Duration(seconds: await audioPlayer.getDuration());
+        });
+      }
+      if(event == PlayerState.STOPPED){
+        setState((){
+          state = PlayerState.STOPPED;
+        });
+      }
+    },onError: (message){
+      print("erreur $message");
+      setState((){
+        state = PlayerState.STOPPED;
+        duration = Duration(seconds: 0);
+        position = Duration(seconds: 0);
+      });
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  Future play() async {
+    await audioPlayer.play(myCurrentMusic.songUrl,isLocal: true);
+    setState(()=>{state = PlayerState.PLAYING});
   }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState( ()=>{ state = PlayerState.PAUSED } );
+  }
+}
+enum ActionMusic {
+  play,
+  pause,
+  rewind,
+  forward
+}
+enum PayerState {
+  playing,
+  stopped,
+  paused
 }
---
+++ lib/music.dart
@@ -0,0 +1,8 @@
+class Music {
+  String title;
+  String artist;
+  String imagePath;
+  String songUrl;
+
+  Music( this.title, this.artist, this.imagePath, this.songUrl);
+}
